f8e777e825250f1a272b8472163bfb9a3c3df89d
rabobahago <rabobahago@gmail.com>
2023-07-17 22:22:46 +0100

    data, features, hooks, services, pages, styles, ui, and utils files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import styled from "styled-components";
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  color: "white";
+  background-color: purple;
+  cursor: pointer;
+  margin: 20px;
+`;
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <H1>The wild Oasis</H1>
+      <Button onClick={() => alert("Check In")}>Chcek In</Button>
+      <Button onClick={() => alert("Check Out")}>Chcek Out</Button>
+    </div>
+  );
 }
