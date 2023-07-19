8149542da61008bbd17dc6dde54db834443450a5
rabobahago <rabobahago@gmail.com>
2023-07-19 13:16:19 +0100

    supabase serverside implemented

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://wjdpetxwxgjjnjptcrsg.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqZHBldHh3eGdqam5qcHRjcnNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk3MzM2NDgsImV4cCI6MjAwNTMwOTY0OH0.7ORatMeZUX25twHDOOiaGgBWesGJ253BKAWHtPgVW7s";
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
