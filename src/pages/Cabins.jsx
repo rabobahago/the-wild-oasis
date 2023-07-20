2f61228787d73cdbc7162bf964b3095a13b70458
rabobahago <rabobahago@gmail.com>
2023-07-20 12:36:41 +0100

    new cabins form created with react hook form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 // import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
-
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -12,6 +15,8 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm(!showForm)}>Add new Cabin</Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
