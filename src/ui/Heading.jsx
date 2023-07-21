dec83807bd069dc19431825d671f8fdfd4cfc3cd
rabobahago <rabobahago@gmail.com>
2023-07-21 14:07:59 +0100

    setting features implemented

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -19,14 +19,14 @@ const Heading = styled.h1`
       font-size: 2rem;
       font-weight: 600;
     `}
-    
+
     ${(props) =>
     props.as === "h3" &&
     css`
       font-size: 2rem;
       font-weight: 500;
     `}
-    
+
   line-height: 1.4;
 `;
 
