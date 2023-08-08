3e3d4b646231808896076407c1551b91cd137c75
rabobahago <rabobahago@gmail.com>
2023-08-08 11:09:19 +0100

    all booking level finished

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -14,6 +14,9 @@ import { useBooking } from "./useBooking";
 import { useNavigate } from "react-router-dom";
 import { HiArrowDownOnSquare, HiArrowUpOnSquare } from "react-icons/hi2";
 import useCheckout from "../check-in-out/useCheckout";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -22,9 +25,10 @@ const HeadingGroup = styled.div`
 `;
 
 function BookingDetail() {
+  const { isDeleting, deleteBooking } = useDeleteBooking();
+
   const navigate = useNavigate();
   const { booking, isLoading } = useBooking();
-  // const status = "checked-in";
 
   const moveBack = useMoveBack();
   const { checkout, isCheckingOut } = useCheckout();
@@ -66,6 +70,23 @@ function BookingDetail() {
             Check out
           </Button>
         )}
+
+        <Modal>
+          <Modal.Open opens="delete">
+            <Button variation="danger">Delete Booking</Button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete
+              resourceName="booking"
+              disabled={isDeleting}
+              onConfirm={() =>
+                deleteBooking(bookingId, {
+                  onSettled: () => navigate(-1),
+                })
+              }
+            />
+          </Modal.Window>
+        </Modal>
 
         <Button variation="secondary" onClick={moveBack}>
           Back
