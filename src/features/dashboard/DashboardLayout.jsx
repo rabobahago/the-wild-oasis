bde9e9e75bb23a7213461818ba5fb172df5a269c
rabobahago <rabobahago@gmail.com>
2023-08-11 14:51:49 +0100

    computed recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,14 +1,19 @@
 import styled from "styled-components";
-
+import Spinner from "../../ui/Spinner";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
-import React from "react";
-
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
 export default function DashboardLayout() {
+  const { isLoading: isLoading1, bookings } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+  console.log(stays);
+  if (isLoading1 || isLoading2) return <Spinner />;
+  console.log(bookings);
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
