2da0564296af564ed3ce4a9b08d71fd659b3ab9f
rabobahago <rabobahago@gmail.com>
2023-08-13 02:22:09 +0100

    Error boundary implemented

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -29,3 +29,24 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+import React from "react";
+import Heading from "./Heading";
+import GlobalStyles from "../styles/GlobalStyles";
+import Button from "./Button";
+
+export default function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong 😢</Heading>
+          {/* <p>{error.message}</p> */}
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try Again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
