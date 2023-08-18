b23b2a1b377bd73eff357ba03e98c7352fae1280
rabobahago <rabobahago@gmail.com>
2023-08-18 09:38:14 +0100

    load dashboard data fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./ui/ErrorFallback.jsx";
-
+//https://the-wild-oasis-rho.vercel.app/dashboard
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary
